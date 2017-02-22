adb5d0714ab408c48cb443ad55279697adb0e476
savybrandt <savybrandt@gmail.com>
2017-02-21 18:53:07 -0800

    login page

---
+++ src/components/Login/LoginView.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router'
+import Title from './../TitleView.jsx'
+import Login from './Login.jsx'
+import Signup from './Signup.jsx'
+
+class LoginView extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      login: true,
+    }
+  }
+
+  switchPage() {
+    this.setState({
+      login: !this.state.login,
+    })
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <Title/>
+        {this.state.login ? (<Login/>) : (<Signup/>)}
+        <p>{this.state.login ? 'New?' : "Alread have an account?"} <a onClick={this.switchPage.bind(this)}>{this.state.login ? "Sign up!" : "Log-in!"}</a></p>
+      </div>
+    );
+  }
+}
+
+export default LoginView;
