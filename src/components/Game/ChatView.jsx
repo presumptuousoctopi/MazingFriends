fd9c8a3cd6d086c967e14d7c64ef6f758044f27e
savybrandt <savybrandt@gmail.com>
2017-02-22 15:05:36 -0800

    progress on switching chat options

--- src/components/Game/ChatView.jsx
+++ src/components/Game/ChatView.jsx
@@ -6,8 +6,15 @@ class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
-      chat: "text"
+      text: null
     }
+  }
+
+  switchChat(value) {
+    this.setState({
+      text: value
+    })
+    console.log(this.state.text)
   }
 
   render() {
@@ -17,11 +24,11 @@ class Chat extends React.Component {
           <h1>Mazing Friends</h1>
           <form>
             <h3>Chat Options</h3>
-            <input type="radio" name="chat" value="text"/>Text
-            <input type="radio" name="chat" value="video"/>Video
+            <input type="radio" name="chat" value="text" onChange={this.switchChat.bind(this, "text")}/>Text
+            <input type="radio" name="chat" value="video" onChange={this.switchChat.bind(this, "video")}/>Video
           </form>
         </div>
-        {this.state.chat === "text" ? <TextChat/> : <VideoChat />}
+        {this.state.text === "text" ? <TextChat/> : <VideoChat/>}
       </div>
     );
   }
