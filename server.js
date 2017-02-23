0e39dd6e330a9310aea2271b20953f388a7e0e2a
DJ Kim <ehdwn1212@gmail.com>
2017-02-22 22:46:12 -0800

    Do not serve get request for bundle.js

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.get('/favicon.ico', function(request, response) {
   response.sendFile(path.join(__dirname, './src/favicon.ico'));
 });
 
-app.get('/build/bundle.js', function(request, response) {
-  response.sendFile(path.join(__dirname, './src/build/bundle.js'));  
-});
+// app.get('/build/bundle.js', function(request, response) {
+//   response.sendFile(path.join(__dirname, './src/build/bundle.js'));  
+// });
 
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, './src/index.html'));
