9cdcee829eda152ff19204d58cc41c1c971fbd7d
DJ Kim <ehdwn1212@gmail.com>
2017-02-23 10:24:04 -0800

    Change database url

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
 var Sequelize = require('sequelize');
 
 var sequelize;
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging:  true //false
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres'
   })
 } else {
   // the application is executed on the local machine ... use mysql
