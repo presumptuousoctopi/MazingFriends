582d885d632c215dc44686c204365626beaf2138
DJ Kim <ehdwn1212@gmail.com>
2017-02-23 19:23:34 -0800

    Add timer

--- src/gameSocket.js
+++ src/gameSocket.js
@@ -1,39 +1,79 @@
 /*******************************************************
  Game Sockets
 *******************************************************/
+
+//data reporter
+  var outputplane = BABYLON.Mesh.CreatePlane("outputplane", 25, scene, false);
+  outputplane.billboardMode = BABYLON.AbstractMesh.BILLBOARDMODE_ALL;
+  outputplane.material = new BABYLON.StandardMaterial("outputplane", scene);
+  outputplane.position = new BABYLON.Vector3(-25, 15, 25);
+  outputplane.scaling.y = 0.4;
+  // outputplane.parent = camera;
+  // outputplane.position = new BABYLON.Vector3(0, 11, 0);
+
+  var outputplaneTexture = new BABYLON.DynamicTexture("dynamic texture", 512, scene, true);
+  outputplane.material.diffuseTexture = outputplaneTexture;
+  outputplane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+  outputplane.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+  outputplane.material.backFaceCulling = false;
+
+  outputplaneTexture.drawText("Timer", null, 140, "bold 140px verdana", "white", "#0000AA");
+
+  var context2D = outputplaneTexture.getContext();
+  var refreshTime = function(data) {
+    context2D.clearRect(0, 200, 512, 512);
+    outputplaneTexture.drawText(data, null, 380, "140px verdana", "white", null);
+  }
+var originalTime = 0;
+var currentTime = 0;
+setInterval( () => {
+  if ( originalTime !== 0 ) {
+    var seconds = Math.round((new Date().getTime() - originalTime) / 100 ) / 10;
+    var minutes = Math.floor(seconds / 60);
+    var seconds = ( !(seconds % 1) ? ( Math.round((seconds % 60) * 10) / 10 + '.0') : Math.round((seconds % 60) * 10) / 10 );
+    var minutes = ( minutes === 0 ? '' : (minutes + ':') );
+    currentTime = minutes + seconds;
+  }
+}, 100);
 // Notify whether user is first or second player and update user position
 socket.on('firstPlayer', function(firstPlayer) {
   window.camera.position = firstPlayerPosition;
   window.camera.rotation = new BABYLON.Vector3(0, 0, 0);
   console.log('You are first player');
   engine.runRenderLoop(function(){
-    console.log('fps : ', engine.fps);
+    // console.log('fps : ', engine.fps);
     var currentCameraPosition = camera.position.x+camera.position.y+camera.position.z;
     if ( currentCameraPosition !== previousCameraPosition ) {
         previousCameraPosition = currentCameraPosition;
         socket.emit('userPositionChanged', camera.position);
     }
-    console.log(window.camera.rotation);
+    if ( currentTime !== 0 ) {
+      refreshTime(currentTime);    
+    }
+    // console.log('currentTime : ', currentTime);
+    // console.log(window.camera.rotation);
     scene.render();
   });
 
 });
 
+var flag = false;
 socket.on('secondPlayer', function(secondPlayer) {
   window.playerType = secondPlayer;
   window.camera.position = mediumLevelSecondPlayerPosition;
   window.camera.rotation = new BABYLON.Vector3(0, 0, 0);
-
+  originalTime = new Date().getTime();
   // Send player position to other player
   socket.emit('sendPlayer', window.camera.position);
   console.log('secondPlayer');
   engine.runRenderLoop(function(){
-    console.log('fps : ', engine.fps);
     var currentCameraPosition = camera.position.x+camera.position.y+camera.position.z;
     if ( currentCameraPosition !== previousCameraPosition ) {
         previousCameraPosition = currentCameraPosition;
         socket.emit('userPositionChanged', camera.position);
     }
+    refreshTime(currentTime);    
+    // console.log(currentTime);
     //console.log(window.camera.rotation);
     scene.render();
   });
@@ -61,6 +101,7 @@ socket.on('serverSendingMaze', function(maze) {
 // Send player position to newly joined player(s)
 socket.on('newPlayerRequestInfo', function() {
   socket.emit('sendPlayer', window.camera.position);
+  originalTime = new Date().getTime();
   console.log('New player joined!');
 });
 
