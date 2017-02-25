95cff835df6f1eb9102fb600fcd53ad2075d557f
savybrandt <savybrandt@gmail.com>
2017-02-24 20:01:58 -0800

    added controls view

--- src/components/Game/ChatView.jsx
+++ src/components/Game/ChatView.jsx
@@ -31,8 +31,8 @@ class Chat extends React.Component {
       <div className="Chat">
         <div className="Options">
           <h1>Mazing Friends</h1>
-          {this.props.timer ? <h2>{this.props.timer}</h2> : <p>waiting for second player</p>}
-          <button className="optionButtons">Controls</button>
+          {this.props.timer ? <h2>{this.props.timer}</h2> : <p>waiting for second player...</p>}
+          <button className="optionButtons" onClick={this.props.controlsClickHandler}>Controls</button>
           <Link to="/"><button className="optionButtons" onClick={this.stopGame.bind(this)}>Quit Game</button></Link>
           <form>
             <h3>Chat Options</h3>
---
+++ src/components/Game/Controls.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Controls extends React.Component {
+  render() {
+    return (
+      <div className="Controls">
+        <h1>Controls</h1>
+
+        <table className="Move">
+          <th>Move</th>
+            <tr>
+              <td> A: left</td>
+            </tr>
+            <tr>
+              <td> D: right</td>
+            </tr>
+            <tr>
+              <td> W: forward</td>
+            </tr>
+            <tr>
+              <td> S: backward</td>
+            </tr>
+            <tr>
+              <td>[ space ]: jump</td>
+            </tr>
+        </table>
+
+         <table className="Look">
+          <th>Look</th>
+            <tr>
+              <td> ← : left</td>
+            </tr>
+            <tr>
+              <td> → : right</td>
+            </tr>
+            <tr>
+              <td> ↑ : up</td>
+            </tr>
+            <tr>
+              <td> ↓ : down</td>
+            </tr>
+            <tr>
+              <td> F : fire</td>
+            </tr>
+        </table>
+
+      </div>
+    );
+  }
+}
+
+export default Controls;
