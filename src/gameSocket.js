736dbecead8688d73d2015a8dfa69ea256721dc2
DJ Kim <ehdwn1212@gmail.com>
2017-02-28 11:33:01 -0800

    Add select level feature

--- src/gameSocket.js
+++ src/gameSocket.js
@@ -6,6 +6,17 @@
 //   // console.log('New percentage : ', percentage);
 //   window.distancePercentage = percentage;
 // });
+setInterval ( function() {
+  if ( !!window.mazeLevel && !!window.otherPlayer ) {
+    var p1 = window.camera.position;
+    var p2 = window.otherPlayer.position;
+    socket.emit('calculateDistance', {
+      p1: p1,
+      p2: p2,
+      level: window.mazeLevel
+    });
+  } 
+}, 3000);
 
 //data reporter
   var outputplane = BABYLON.Mesh.CreatePlane("outputplane", 25, scene, false);
@@ -63,6 +74,7 @@ var protocolPrefix = (window.location.protocol === 'https:') ? 'wss:' : 'ws:';
 // Notify whether user is first or second player and update user position
 window.inRoom = false;
 socket.on('firstPlayer', function(firstPlayer) {
+
   window.inRoom = true;
   window.camera.position = firstPlayerPosition;
   window.camera.rotation = new BABYLON.Vector3(-0.38385, -.77694, 0);
@@ -72,7 +84,7 @@ socket.on('firstPlayer', function(firstPlayer) {
     outputplane2.position = new BABYLON.Vector3(20 + camera.position.x, 20 +camera.position.y, 20 + camera.position.z);
     var xGrid = Math.floor(window.camera.position.x / 4 + .4);
     var yGrid = Math.floor(window.camera.position.z / 4 + .4);
-    console.log('Here are grids : ', xGrid, yGrid);
+    // console.log('Here are grids : ', xGrid, yGrid);
     // console.log('fps : ', engine.fps);
     var currentCameraPosition = camera.position.x+camera.position.y+camera.position.z;
     if ( currentCameraPosition !== previousCameraPosition ) {
@@ -87,10 +99,6 @@ socket.on('firstPlayer', function(firstPlayer) {
       var p2 = window.otherPlayer.position;
       // Calculate distance between two users
 
-      socket.emit('calculateDistance', {
-        p1: p1,
-        p2: p2
-      });
       var distanceBetweenUsers = window.calculateDistance(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
       if ( distanceBetweenUsers < 3 ) {
         window.finishGame();
@@ -112,7 +120,7 @@ socket.on('secondPlayer', function(secondPlayer) {
   window.camera.keysLeft = [65]; 
   window.camera.keysRight = [68];
 
-  window.camera.position = mediumLevelSecondPlayerPosition;
+
   window.camera.rotation = new BABYLON.Vector3(-0.38385, -.77694, 0);
   originalTime = new Date().getTime();
   // Send player position to other player
@@ -134,11 +142,6 @@ socket.on('secondPlayer', function(secondPlayer) {
     if ( window.otherPlayer && window.finished === false ) {
       var p1 = window.camera.position;
       var p2 = window.otherPlayer.position;
-
-      socket.emit('calculateDistance', {
-        p1: p1,
-        p2: p2
-      });
       // Calculate distance between two users
       var distanceBetweenUsers = window.calculateDistance(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
  
@@ -166,9 +169,30 @@ socket.on('roomName', function(roomName) {
 });
 
 // Receive a custom map from server and build the maze
-socket.on('serverSendingMaze', function(maze) {
+socket.on('serverSendingMaze', function(mazeData) {
+  var maze = mazeData.maze;
+  console.log('Here is mazeData : ', mazeData);
+  window.mazeLevel = mazeData.mazeLevel.toString();
+  window.mazeSize;
+  if ( window.playerType === 'secondPlayer' ) {
+    window.camera.position = window.secondPlayerPositions[mazeData.mazeLevel];
+  }
+  if ( window.mazeLevel === '1' ) {
+    window.mazeSize = 5;
+  } else if ( window.mazeLevel === '2' ) {
+    window.mazeSize = 10;
+  } else if ( window.mazeLevel === '3' ) {
+    window.mazeSize = 20;
+  }
+  // window.secondPlayerPosition = secondPlayerPositions[mazeSize];
+  window.groundBoundaryLength = window.boundaryWalls[window.mazeSize][1] * 1.2;
+  const a = window.boundaryWalls[window.mazeSize][0];
+  const b = window.boundaryWalls[window.mazeSize][1];
+  window.farthestUserDist = Math.pow(a * a + b * b, (1/2));
   buildSimpleMaze(maze);
   console.log('Received a maze from server');
+  window.ground = addGround();
+  buildBoundaryWalls(boxLength, window.scene);
 });
 
 // Send player position to newly joined player(s)
@@ -180,6 +204,7 @@ socket.on('newPlayerRequestInfo', function() {
   window.camera.keysLeft = [65]; 
   window.camera.keysRight = [68];
   console.log('New player joined!');
+
 });
 
 // Update all other users' positions
