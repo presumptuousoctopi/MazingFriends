4574e4be489ae02bc39c36379e847c332558d4e3
DJ Kim <ehdwn1212@gmail.com>
2017-02-28 11:33:01 -0800

    Add select level feature

--- src/gameSocket.js
+++ src/gameSocket.js
@@ -193,6 +193,7 @@ socket.on('serverSendingMaze', function(mazeData) {
   console.log('Received a maze from server');
   window.ground = addGround();
   buildBoundaryWalls(boxLength, window.scene);
+<<<<<<< HEAD
   if ( window.mazeLevel === '3' ) {
     window.createGhost();
     window.createGhost();
@@ -200,6 +201,8 @@ socket.on('serverSendingMaze', function(mazeData) {
     window.createGhost();    
   }
   window.createGhost();  
+=======
+>>>>>>> Add select level feature
 });
 
 // Send player position to newly joined player(s)
