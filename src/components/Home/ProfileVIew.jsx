9151afe2e86e75d675ed79fe967d7a77884f931b
savybrandt <savybrandt@gmail.com>
2017-03-02 15:15:03 -0800

    started on profile view, fixed some lobby css

--- src/components/Home/ProfileVIew.jsx
+++ src/components/Home/ProfileVIew.jsx
@@ -4,9 +4,29 @@ import FriendSearch from './FriendSearch.jsx'
 
 
 class Profile extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			currentUser: null
+		};
+	}
+
+	componentDidMount() {
+		var context = this;
+		socket.on('currentUser', function(user) {
+			context.setState({
+				currentUser: user
+			});
+		});
+	}
+
   render() {
     return (
       <div className="Profile">
+      	<div className="profileHeader">
+	      	<h1>Mazing Friends </h1>
+	      	<h2>{this.state.currentUser}</h2>
+      	</div>
      	  <FriendSearch/>
       	<UserStats />
       </div>
